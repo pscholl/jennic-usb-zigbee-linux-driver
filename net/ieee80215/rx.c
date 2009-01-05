c9843d2e4cb717d115dced83dd4aaccb16d3be6d
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2009-01-05 18:35:10 +0300

    further cleanup rx part
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- net/ieee80215/rx.c
+++ net/ieee80215/rx.c
@@ -32,72 +32,35 @@
 	} \
 }
 
-#if 0
-void ieee80215_rx(struct ieee80215_dev *dev, struct sk_buff *skb)
-{
-	struct ieee80215_priv *priv = ieee80215_to_priv(dev);
-	struct net_device *master = priv->master, *prev = master;
-	struct ieee80215_netdev_priv *ndp;
-
-	BUG_ON(!skb);
-
-	skb->dev = master;
-	skb->protocol = htons(ETH_P_IEEE80215_MAC);
-
-	if (skb->len < 3 + 4 + 2)
-		goto out_last;
-
-	// FIXME: correct address checking
-	unsigned char *head = skb->data;
-	skb_pull(skb, 3+8+8);
-	DBG_DUMP(head+3+8, 8);
-	// FIXME: check CRC if necessary
-	skb_trim(skb, skb->len - 2); // CRC
-	skb->iif = master->ifindex;
-
-	list_for_each_entry_rcu(ndp, &priv->slaves, list)
-	{
-		DBG_DUMP(ndp->dev->dev_addr, 8);
-		if (memcmp(head + 3 + 8 , ndp->dev->dev_addr, IEEE80215_ADDR_LEN) &&
-		    memcmp(head + 3 + 8 , ndp->dev->broadcast, IEEE80215_ADDR_LEN))
-			continue;
-
-		if (prev) {
-			struct sk_buff *skb2 = skb_clone(skb, GFP_ATOMIC);
-			skb2->dev = prev;
-			netif_rx(skb2);
-
-			skb->protocol = htons(ETH_P_IEEE80215);
-		}
-
-		prev = ndp->dev;
-	}
-
-out_last:
-	if (prev) {
-		skb->dev = prev;
-		netif_rx(skb);
-		skb = NULL;
-	} else
-		dev_kfree_skb(skb);
-}
-#else
 void ieee80215_rx(struct ieee80215_dev *dev, struct sk_buff *skb)
 {
 	struct ieee80215_priv *priv = ieee80215_to_priv(dev);
 	struct ieee80215_netdev_priv *ndp;
 	unsigned char *head;
+	unsigned int head_off, tail_off;
 
 	BUG_ON(!skb);
 
-	head = skb->data;
-
-	skb->dev = priv->master;
-	skb->protocol = htons(ETH_P_IEEE80215_MAC);
 	skb->iif = skb->dev->ifindex;
 
 	if (skb->len < /*3 + 4 + 2*/ 3 + 8 + 8 + 2)
 		goto out_master;
+
+	/*
+	 * We parse headers on our origin SKB, changing pointers.
+	 * Then we can restore them back just before passing the SKB
+	 * to netif_rx(master) for delivery.
+	 */
+
+	// FIXME: correct address checking
+	head_off = 3 + IEEE80215_ADDR_LEN + IEEE80215_ADDR_LEN;
+	head = skb->data;
+	skb_pull(skb, head_off);
+	DBG_DUMP(head+3+IEEE80215_ADDR_LEN, 8);
+
+	// FIXME: check CRC if necessary
+	tail_off = 2;
+	skb_trim(skb, skb->len - tail_off); // CRC
 
 	list_for_each_entry_rcu(ndp, &priv->slaves, list)
 	{
@@ -105,27 +68,23 @@ void ieee80215_rx(struct ieee80215_dev *dev, struct sk_buff *skb)
 		DBG_DUMP(ndp->dev->dev_addr, 8);
 
 		// FIXME: correct address checking
-		if (memcmp(head + 3 + 8 , ndp->dev->dev_addr, IEEE80215_ADDR_LEN) &&
-		    memcmp(head + 3 + 8 , ndp->dev->broadcast, IEEE80215_ADDR_LEN))
+		if (memcmp(head + 3 + IEEE80215_ADDR_LEN , ndp->dev->dev_addr, IEEE80215_ADDR_LEN) &&
+		    memcmp(head + 3 + IEEE80215_ADDR_LEN , ndp->dev->broadcast, IEEE80215_ADDR_LEN))
 			continue;
 
 		skb2 = skb_clone(skb, GFP_ATOMIC);
 		skb_reset_mac_header(skb2);
 
-		// FIXME: correct address checking
-		skb_pull(skb2, 3+8+8);
-		DBG_DUMP(head+3+8, 8);
-
-		// FIXME: check CRC if necessary
-		skb_trim(skb2, skb2->len - 2); // CRC
-
 		skb2->dev = ndp->dev;
 		skb2->protocol = htons(ETH_P_IEEE80215);
-
 		netif_rx(skb2);
 	}
 
+	skb_push(skb, head_off);
+	skb_put(skb, tail_off);
+
 out_master:
+	skb->dev = priv->master;
+	skb->protocol = htons(ETH_P_IEEE80215_MAC);
 	netif_rx(skb);
 }
-#endif
