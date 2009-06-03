363598f83793613e5addd3e5fb0bf8123e2a4c39
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2009-06-03 16:17:43 +0400

    nl802154: split policy away to separate source file
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- include/net/ieee802154/nl802154.h
+++ include/net/ieee802154/nl802154.h
@@ -21,6 +21,10 @@
 #ifndef IEEE802154_NL_H
 #define IEEE802154_NL_H
 
+#ifdef __KERNEL__
+#include <net/netlink.h>
+#endif
+
 #define IEEE802154_NL_NAME "802.15.4 MAC"
 #define IEEE802154_MCAST_COORD_NAME "coordinator"
 #define IEEE802154_MCAST_BEACON_NAME "beacon"
@@ -72,38 +76,7 @@ enum {
 #define NLA_GET_HW_ADDR(attr, addr) do { u64 _temp = nla_get_u64(attr); memcpy(addr, &_temp, 8); } while (0)
 #define NLA_PUT_HW_ADDR(msg, attr, addr) do { u64 _temp; memcpy(&_temp, addr, 8); NLA_PUT_U64(msg, attr, _temp); } while (0)
 
-#ifdef IEEE802154_NL_WANT_POLICY
-static struct nla_policy ieee802154_policy[IEEE802154_ATTR_MAX + 1] = {
-	[IEEE802154_ATTR_DEV_NAME] = { .type = NLA_STRING, },
-	[IEEE802154_ATTR_DEV_INDEX] = { .type = NLA_U32, },
-
-	[IEEE802154_ATTR_STATUS] = { .type = NLA_U8, },
-	[IEEE802154_ATTR_SHORT_ADDR] = { .type = NLA_U16, },
-	[IEEE802154_ATTR_HW_ADDR] = { .type = NLA_HW_ADDR, },
-	[IEEE802154_ATTR_PAN_ID] = { .type = NLA_U16, },
-	[IEEE802154_ATTR_CHANNEL] = { .type = NLA_U8, },
-	[IEEE802154_ATTR_COORD_SHORT_ADDR] = { .type = NLA_U16, },
-	[IEEE802154_ATTR_COORD_HW_ADDR] = { .type = NLA_HW_ADDR, },
-	[IEEE802154_ATTR_COORD_PAN_ID] = { .type = NLA_U16, },
-	[IEEE802154_ATTR_SRC_SHORT_ADDR] = { .type = NLA_U16, },
-	[IEEE802154_ATTR_SRC_HW_ADDR] = { .type = NLA_HW_ADDR, },
-	[IEEE802154_ATTR_SRC_PAN_ID] = { .type = NLA_U16, },
-	[IEEE802154_ATTR_DEST_SHORT_ADDR] = { .type = NLA_U16, },
-	[IEEE802154_ATTR_DEST_HW_ADDR] = { .type = NLA_HW_ADDR, },
-	[IEEE802154_ATTR_DEST_PAN_ID] = { .type = NLA_U16, },
-
-	[IEEE802154_ATTR_CAPABILITY] = { .type = NLA_U8, },
-	[IEEE802154_ATTR_REASON] = { .type = NLA_U8, },
-	[IEEE802154_ATTR_SCAN_TYPE] = { .type = NLA_U8, },
-	[IEEE802154_ATTR_CHANNELS] = { .type = NLA_U32, },
-	[IEEE802154_ATTR_DURATION] = { .type = NLA_U8, },
-#ifdef __KERNEL__
-	[IEEE802154_ATTR_ED_LIST] = { .len = 27 },
-#else
-	[IEEE802154_ATTR_ED_LIST] = { .minlen = 27, .maxlen = 27 },
-#endif
-};
-#endif
+extern struct nla_policy ieee802154_policy[];
 
 /* commands */
 /* REQ should be responded with CONF
