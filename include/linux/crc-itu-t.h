dd3ab3098759d796508e5c02f3f450dcf58f8d56
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2009-07-27 15:35:55 +0400

    Revert "crc-itu-t: add bit-reversed calculation"
    This reverts commit 71d2d1270d54c4834cb45c9908bcccd5c64b9fce.
    
    Drop bireversed CRC-ITU-T as it's used now by our tree.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- include/linux/crc-itu-t.h
+++ include/linux/crc-itu-t.h
@@ -6,9 +6,6 @@
  *   Poly  0x0x1021 (x^16 + x^12 + x^15 + 1)
  *   Init  0
  *
- * The bit-reversed buffer variants may be non-standard, but some firmware
- * loaders require them.
- *
  * This source code is licensed under the GNU General Public License,
  * Version 2. See the file COPYING for more details.
  */
@@ -17,21 +14,14 @@
 #define CRC_ITU_T_H
 
 #include <linux/types.h>
-#include <linux/bitrev.h>
 
 extern u16 const crc_itu_t_table[256];
 
 extern u16 crc_itu_t(u16 crc, const u8 *buffer, size_t len);
-extern u16 crc_itu_t_bitreversed(u16 crc, const u8 *buffer, size_t len);
 
 static inline u16 crc_itu_t_byte(u16 crc, const u8 data)
 {
 	return (crc << 8) ^ crc_itu_t_table[((crc >> 8) ^ data) & 0xff];
-}
-
-static inline u16 crc_itu_t_bitreversed_byte(u16 crc, const u8 data)
-{
-	return (crc << 8) ^ crc_itu_t_table[((crc >> 8) ^ bitrev8(data)) & 0xff];
 }
 
 #endif /* CRC_ITU_T_H */
